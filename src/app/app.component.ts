fa568b8cd15a081989ecb0efa031b633669f2191
manoj <107401848+manojtatipamula@users.noreply.github.com>
2024-06-08 16:19:21 +0530

    add angular material and try out address form (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import  {AddressFormComponent} from './address-form/address-form.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatSlideToggleModule, AddressFormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'electronics-world-frontend';
