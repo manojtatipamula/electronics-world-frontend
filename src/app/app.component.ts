fa7759b5535172b4989f09f7b18302927d11cc5f
Manoj Tatipamula <manojtatipamula@gmail.com>
2024-06-08 18:42:54 +0530

    re-organize the address-form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
-import  {AddressFormComponent} from './address-form/address-form.component'
+import  {AddressFormComponent} from './components/address-form/address-form.component'
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AddressFormComponent } from './components/address-form/address-form.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '' , component: AppComponent , pathMatch: 'full'
+},{
+    path : 'home' , component : AddressFormComponent
+}];
